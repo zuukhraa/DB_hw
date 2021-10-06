1b452ee3dca604b9b375b68c98e8b157c304760d
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-10-06 15:17:57 +0300

    Update V11__Insert_into_book.sql

--- Scripts/Insert/V11__Insert_into_book.sql
+++ Scripts/Insert/V11__Insert_into_book.sql
@@ -6,7 +6,3 @@ values ('Pride and Prejudice', '1813', 2, 1, 1);
 
 insert into book (name, yearOfPublishing, author_id, publishing_house_id, genre_department_id)
 values ('Anna Karenina', '1878', 3, 2, 1);
-
-
-
-
