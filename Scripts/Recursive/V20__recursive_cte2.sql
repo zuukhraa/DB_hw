3f10a7df1e68c4d3083f950d52b478c110c5ab1c
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-10-18 18:49:24 +0300

    add

--- Scripts/Recursive/V20__recursive_cte2.sql
+++ Scripts/Recursive/V20__recursive_cte2.sql
@@ -1,5 +1,6 @@
 with recursive cte2 as (
-select book_id, name, yearOfPublishing, author_id, publishing_house_id, genre_department_id from book where author_id = 1
+select book_id, name, yearOfPublishing, author_id, publishing_house_id, genre_department_id from book 
+where author_id = 1
 union 
 select book.book_id, book.name, book.yearOfPublishing, book.author_id, book.publishing_house_id, book. genre_department_id from book
 join cte2 on book.author_id = cte2.author_id
