482428eb118917bcf595408629ea0a80471b88ab
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-11-08 23:44:47 +0300

    add constraint

--- Scripts/Create table/V1__Create_author_table.sql
+++ Scripts/Create table/V1__Create_author_table.sql
@@ -1,5 +1,5 @@
 create table author (
                         author_id serial primary key,
-                        first_name varchar(255) not null,
-                        last_name varchar(255) not null
+                        first_name varchar(255) check(char_length(first_name) > 1),
+                        last_name varchar(255) check(char_length(last_name) > 1)
 );
