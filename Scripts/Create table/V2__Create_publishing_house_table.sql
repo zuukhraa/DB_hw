e4386a51c333079eac264e691691660d3ae52589
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-11-08 23:52:20 +0300

    add constraint

--- Scripts/Create table/V2__Create_publishing_house_table.sql
+++ Scripts/Create table/V2__Create_publishing_house_table.sql
@@ -1,8 +1,8 @@
 create table publishing_house (
                                   publishing_house_id serial primary key,
-                                  name varchar(255) not null,
-                                  city varchar(100) not null,
-                                  telephoneNumber integer not null,
-                                  email varchar(100) not null,
-                                  website varchar(100) not null
+                                  name varchar(255) check(char_length(name) > 1),
+                                  city varchar(100) check(char_length(city) > 1),
+                                  telephoneNumber integer constraint number_length check(telephoneNumber > 0),
+                                  email varchar(100) check(char_length(email) > 1),
+                                  website varchar(100) check(char_length(website) > 1)
 );
