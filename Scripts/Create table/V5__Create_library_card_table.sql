f09305f5335fc47799884e17a6bf942998d2930a
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-11-09 00:05:02 +0300

    add constraint

--- Scripts/Create table/V5__Create_library_card_table.sql
+++ Scripts/Create table/V5__Create_library_card_table.sql
@@ -1,7 +1,7 @@
 create table library_card (
                               library_card_id serial primary key,
-                              visitor_firstName varchar(255) not null,
-                              visitor_lastName varchar(255) not null,
-                              passport integer not null,
-                              home_address varchar (75) not null
+                              visitor_firstName varchar(255) check(char_length(visitor_firstName) > 1),
+                              visitor_lastName varchar(255) check(char_length(visitor_lastName) > 1),
+                              passport integer constraint passport_length check(passport = 10),
+                              home_address varchar (75) check(char_length(home_address) > 1)
 );
