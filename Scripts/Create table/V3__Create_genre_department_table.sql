43cb854030093efba82fb4f3b03f53b817e80848
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-11-08 23:59:08 +0300

    add constraint

--- Scripts/Create table/V3__Create_genre_department_table.sql
+++ Scripts/Create table/V3__Create_genre_department_table.sql
@@ -1,4 +1,4 @@
 create table genre_department (
                                   genre_department_id serial primary key,
-                                  name varchar (25) not null
+                                  name varchar (25) check(char_length(name) > 1)
 );
