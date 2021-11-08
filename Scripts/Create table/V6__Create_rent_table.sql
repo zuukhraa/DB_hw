af739f4435ea9484f1ed645c3f1670df75115f83
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-11-09 00:07:19 +0300

    add constraint

--- Scripts/Create table/V6__Create_rent_table.sql
+++ Scripts/Create table/V6__Create_rent_table.sql
@@ -1,7 +1,7 @@
  create table rent (
      id serial primary key,
-     data_of_issue varchar(20) not null,
-     return_date varchar(20) not null,
+     data_of_issue varchar(20) check(char_length(data_of_issue) > 1),
+     return_date varchar(20) check(char_length(return_date) > 1),
      library_card_id integer not null,
      foreign key (library_card_id) references library_card (library_card_id)
  );
